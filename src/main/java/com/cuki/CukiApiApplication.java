7313a05b525b4a92aebeb11dcddb4c36fb426720
conagreen <icoqwee@gmail.com>
2021-08-02 11:19:11 +0900

    first init

---
+++ src/main/java/com/cuki/CukiApiApplication.java
@@ -0,0 +1,13 @@
+package com.cuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CukiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuki/CukiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

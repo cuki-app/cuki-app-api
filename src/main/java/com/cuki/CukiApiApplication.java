8270af5c7d94d406df1360c9991b4f36aee13d52
kellykang-tech <kellykang13@gmail.com>
2021-08-19 22:52:44 +0900

    Schedule 클래스 생성, BaseTimeEntity 클래스 생성

--- src/main/java/com/cuki/CukiApiApplication.java
+++ src/main/java/com/cuki/CukiApiApplication.java
@@ -2,7 +2,9 @@ package com.cuki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CukiApiApplication {
 

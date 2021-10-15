559f94cbbe2ac64ce96da64175e110c1665e0cd1
conagreen <icoqwee@gmail.com>
2021-10-15 16:49:35 +0900

    Controller에 Swagger 설정 코드 적용

--- src/main/java/com/cuki/domain/comment/controller/CommentController.java
+++ src/main/java/com/cuki/domain/comment/controller/CommentController.java
@@ -6,11 +6,14 @@ import com.cuki.domain.comment.dto.SuccessfullyDeletedCommentResponseDto;
 import com.cuki.domain.comment.dto.SuccessfullyRegisteredCommentResponseDto;
 import com.cuki.domain.comment.service.CommentService;
 import com.cuki.global.common.response.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"댓글 API"})
 @RequestMapping("/comments")
 @RestController
 public class CommentController {
@@ -22,19 +25,19 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    // 댓글 등록
+    @ApiOperation(value = "댓글등록")
     @PostMapping
     public ApiResponse<SuccessfullyRegisteredCommentResponseDto> registerComment(@RequestBody RegisterCommentRequestDto registerCommentRequestDto) {
         return ApiResponse.ok(commentService.registerComment(registerCommentRequestDto));
     }
 
-    // 댓글 조회 - 특정 게시물 기준
+    @ApiOperation(value = "댓글 조회 - 게시물 기준")
     @GetMapping("/{scheduleId}")
     public ApiResponse<List<CommentResponseDto>> getComments(@PathVariable Long scheduleId) {
         return ApiResponse.ok(commentService.getComments(scheduleId));
     }
 
-    // 댓글 삭제
+    @ApiOperation(value = "댓글 삭제")
     @DeleteMapping("/{commentId}")
     public ApiResponse<SuccessfullyDeletedCommentResponseDto> deleteComment(@PathVariable Long commentId) {
         return ApiResponse.ok(commentService.deleteComment(commentId));

c54eef1a405f094bd85324a0a815ad4b5510c275
conagreen <icoqwee@gmail.com>
2021-11-03 22:33:24 +0900

    예외 적용 - AuthenticationNotFoundException

--- src/main/java/io/cuki/global/util/SecurityUtil.java
+++ src/main/java/io/cuki/global/util/SecurityUtil.java
@@ -1,8 +1,11 @@
 package io.cuki.global.util;
 
+import io.cuki.domain.member.exception.AuthenticationNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class SecurityUtil {
@@ -13,10 +16,10 @@ public class SecurityUtil {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getName() == null) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+            throw new AuthenticationNotFoundException("Security Context에 인증 정보가 없습니다.");
         }
 
-        log.info("authentication.getName() : " + authentication.getName());
+        log.debug("authentication.getName() -> {}" + authentication.getName());
 
         return Long.parseLong(authentication.getName());
     }

f4d74819c014b807f8aecf8e9fea1378fe471af9
conagreen <icoqwee@gmail.com>
2021-11-03 16:17:04 +0900

    @Getter 어노테이션 추가

--- src/main/java/io/cuki/domain/comment/dto/SuccessfullyRegisteredCommentResponseDto.java
+++ src/main/java/io/cuki/domain/comment/dto/SuccessfullyRegisteredCommentResponseDto.java
@@ -3,6 +3,7 @@ package io.cuki.domain.comment.dto;
 import lombok.*;
 
 @Builder
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SuccessfullyRegisteredCommentResponseDto {
     private Long commentId;

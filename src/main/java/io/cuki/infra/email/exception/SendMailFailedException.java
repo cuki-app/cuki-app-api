7f06122c3eb6a9de9fe1a476419616c38fbe393d
conagreen <icoqwee@gmail.com>
2021-11-03 22:09:57 +0900

    메일 인증 예외 클래스 정의

--- src/main/java/io/cuki/infra/email/exception/SendMailFailedException.java
+++ src/main/java/io/cuki/infra/email/exception/SendMailFailedException.java
@@ -1,6 +1,6 @@
 package io.cuki.infra.email.exception;
 
-public class SendMailFailedException extends Throwable {
+public class SendMailFailedException extends RuntimeException {
     public SendMailFailedException(String message) {
         super(message);
     }

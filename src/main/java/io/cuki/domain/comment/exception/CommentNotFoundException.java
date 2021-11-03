74877dde7c6f44fd347f8f3bca76e1e0e419fba4
conagreen <icoqwee@gmail.com>
2021-11-03 18:51:00 +0900

    댓글 예외 클래스 정의

---
+++ src/main/java/io/cuki/domain/comment/exception/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package io.cuki.domain.comment.exception;
+
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException() {
+        super("존재하지 않는 댓글입니다.");
+    }
+}

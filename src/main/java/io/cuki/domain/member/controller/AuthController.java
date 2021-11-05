ddc0b63bec2491ce48a87b9b557de9999ebe849f
conagreen <icoqwee@gmail.com>
2021-11-06 02:54:58 +0900

    Swagger API 설명 설정 수정

--- src/main/java/io/cuki/domain/member/controller/AuthController.java
+++ src/main/java/io/cuki/domain/member/controller/AuthController.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@Api(tags = {"회원 인증 관련 API"})
+@Api(tags = {"회원 및 인증 관련 API"})
 @Slf4j
 @RestController
 @RequiredArgsConstructor

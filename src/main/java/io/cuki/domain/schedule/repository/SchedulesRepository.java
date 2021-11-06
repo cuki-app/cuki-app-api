a6afcce899ffd8e7289b892cfb82f3e19d742510
kellykang-tech <kellykang13@gmail.com>
2021-11-07 04:14:26 +0900

    Add: 게시글 전체 조회 - page 처리 오류 수정

--- src/main/java/io/cuki/domain/schedule/repository/SchedulesRepository.java
+++ src/main/java/io/cuki/domain/schedule/repository/SchedulesRepository.java
@@ -2,9 +2,14 @@ package io.cuki.domain.schedule.repository;
 
 import io.cuki.domain.schedule.entity.Schedule;
 import io.cuki.domain.schedule.entity.ScheduleStatus;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SchedulesRepository extends JpaRepository<Schedule, Long> {
@@ -14,5 +19,7 @@ public interface SchedulesRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByStatus(ScheduleStatus status);
 
     Slice<Schedule> findBy(Pageable pageable);
+
+    Slice<Schedule> findByCreatedDate(@Param("createdDate") LocalDateTime createdDate, Pageable pageable);
 
 }

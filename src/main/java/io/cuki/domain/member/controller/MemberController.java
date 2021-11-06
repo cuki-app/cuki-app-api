9ceaa6aa5c42210b7a68c09dd167282948b74aa0
conagreen <icoqwee@gmail.com>
2021-11-06 18:27:58 +0900

    Swagger API 설명 설정 수정

--- src/main/java/io/cuki/domain/member/controller/MemberController.java
+++ src/main/java/io/cuki/domain/member/controller/MemberController.java
@@ -4,10 +4,12 @@ import io.cuki.domain.member.service.MemberService;
 import io.cuki.global.common.response.ApiResponse;
 import io.cuki.domain.member.dto.MemberInfoForAdminResponseDto;
 import io.cuki.domain.member.dto.MemberInfoResponseDto;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"회원 관련 API"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/members")
